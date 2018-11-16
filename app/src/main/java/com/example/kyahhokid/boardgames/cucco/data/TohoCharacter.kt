d1c2d1837df71d0f8a3c58706449552c175ecb9b
MYCOMPUTER\kyahhokid <kyahhokid@me.com>
2018-11-16 21:44:22 +0900

    プレイヤーのモデルを追加 #5

---
+++ app/src/main/java/com/example/kyahhokid/boardgames/cucco/data/TohoCharacter.kt
@@ -0,0 +1,8 @@
+package com.example.kyahhokid.boardgames.cucco.data
+
+/**
+ * キャラクター情報種別
+ */
+enum class TohoCharacter {
+    flandle
+}
---
+++ app/src/main/java/com/example/kyahhokid/boardgames/cucco/model/Player.kt
@@ -0,0 +1,12 @@
+package com.example.kyahhokid.boardgames.cucco.model
+
+import com.example.kyahhokid.boardgames.cucco.data.Card
+import com.example.kyahhokid.boardgames.cucco.data.TohoCharacter
+
+/**
+ * プレイヤー
+ */
+class Player(private val myCharacter: TohoCharacter) {
+    // 所持カード
+    private var myCard: Card? = null
+}
